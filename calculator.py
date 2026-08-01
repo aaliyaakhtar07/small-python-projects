538caa1f3959ce2de82d74c9aac0651960504768
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-08-01 22:01:53 +0530

    Python Calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Calculator
+operator = input("Enter an operator (+, -, *, /): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print("Invalid operator")
+print("Result:", result)
