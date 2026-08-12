cf27aa04a402c678349fb8f7d7a97da81bac38bc
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-08-12 18:27:56 +0530

    Update Stock

--- inventory_management_system.py
+++ inventory_management_system.py
@@ -5,7 +5,21 @@ def add_item(item, price, stock):
     else:
         inventory[item] = {'price': price, 'stock': stock}
         print(f"Item '{item}' added successfully.")
+def update_stock(item, quantity):
+    if item not in inventory:
+        print(f"Error: Item '{item}' not found.")
+    else:
+        new_stock = inventory[item]['stock']+quantity
+        if new_stock<0:
+            print(f"Error: Insufficient stock for '{item}'.")
+        else:
+            inventory[item]['stock']=new_stock
+            print(f"Stock for '{item}' updated successfully.")
 add_item("Apple", 0.5, 100)
 add_item("Banana", 0.2, 50)
-add_item("Apple", 0.6, 30)  # Should print an error  
+add_item("Apple", 0.6, 30)  # Should print an error
+update_stock("Apple", -20)
+update_stock("Banana", 30)
+update_stock("Orange", 10)  # Should print an error
+update_stock("Apple", -90)
 print(inventory)
