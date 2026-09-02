0cd2205ec6eb209a4381591e061dcea576fd175d
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-09-02 22:24:59 +0530

    Encryption

--- encryption_program.py
+++ encryption_program.py
@@ -3,4 +3,13 @@ import random
 import string
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
-print(chars)
+key = chars.copy()
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key: {key}")
+
+#ENCRYPTION
+plain_text = input("Enter the text to encrypt: ")
+cipher_text = ""
+
