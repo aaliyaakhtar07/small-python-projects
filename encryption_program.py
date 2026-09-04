253cfd44af748bdfed9514b0ceed537598d3e3ef
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-09-04 19:23:54 +0530

    Encryption Program

--- encryption_program.py
+++ encryption_program.py
@@ -6,10 +6,15 @@ chars = list(chars)
 key = chars.copy()
 random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key: {key}")
+#print(f"chars: {chars}")
+#print(f"key: {key}")
 
 #ENCRYPTION
 plain_text = input("Enter the text to encrypt: ")
 cipher_text = ""
 
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+print(f"Original Text: {plain_text}")
+print(f"Encrypted Text: {cipher_text}")
