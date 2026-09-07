a55b9031ccec3ad88010f71166798aaea72beefc
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-09-07 19:47:43 +0530

    Credit card validator

---
+++ credit_card_validator.py
@@ -0,0 +1,7 @@
+#Credit Card Validator
+sum_odd_digits = 0
+sum_even_digits = 0
+total = 0
+card_number = input("Enter your credit card number: ")
+card_number = card_number.replace("-", "")
+print(f"Card Number: {card_number}")
