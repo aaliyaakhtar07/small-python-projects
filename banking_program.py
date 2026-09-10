7a79fcbb45e5528eb21845af9db81118934ac1da
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-09-10 23:50:35 +0530

    Banking Program

---
+++ banking_program.py
@@ -0,0 +1,20 @@
+#Banking Program
+def show_balance():
+    pass
+
+def deposit():
+    pass
+
+def withdraw():
+    pass
+
+balance = 0
+is_running = True
+
+while is_running:
+    print("Welcome to the Banking Program")
+    print("1. Show Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+    choice = input("Enter your choice (1-4): ") 
