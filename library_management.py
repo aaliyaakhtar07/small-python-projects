2bde91d40b89cab77bf69f134f206ec450143712
aaliyaakhtar07 <aaliyaakhtar07@gmail.com>
2026-09-24 21:23:32 +0530

    Removed Library Management Program

--- library_management.py
+++
@@ -1,9 +0,0 @@
-#Library Management
-class Library:
-    def __init__(self, name):
-        self.name = name
-        self.books = []
-        self.users = []
-
-    def __str__(self):
-        return f"{self.name} Library with {len(self.books)} books and {len(self.users)} users"
